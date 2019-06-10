161d93f83b1681267d392b1b8613b933e39c940a
Carol <carolcavalcante.002@gmail.com>
2019-06-10 17:55:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            float saldo;
+
+            void cadastrar()
+            {
+                Console.Write("Para fazermos o seu cadastro é necessario que informe o seu nome, por favor:  ");
+                nome = Console.ReadLine();
+                Console.Write("informe o valor que ira depositar na conta: ");
+                saldo = float.Parse(Console.ReadLine());
+
+            }
+
+            float ConsultarSaldo()
+            {
+
+                return saldo;
+            }
+
+            void depositar(float valor)
+            {
+                saldo = valor + ConsultarSaldo();
+            }
+
+            void sacar(float valor)
+            {
+                if (valor <= ConsultarSaldo())
+
+                    saldo = ConsultarSaldo() - valor;
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente");
+                }
+            }
+
+
+            cadastrar();
+            Console.WriteLine("");
+            Console.WriteLine(ConsultarSaldo());
+            depositar(50);
+            Console.WriteLine(ConsultarSaldo());
+            sacar(25);
+            Console.WriteLine(ConsultarSaldo());
+            sacar(234);
+
+
+            Console.WriteLine("");
+            Console.ReadKey();
+
+        }
+    }
+}
